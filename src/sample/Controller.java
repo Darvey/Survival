3d27605eadb17e3055beb1744242b8927f7c4308
slentis <cedric.lanoir@gmail.com>
2016-01-13 12:14:22 +0100

    0.0.2 - controller
    test d'écoute du clavier (ZQSD) + println dans le log

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,46 @@
 package sample;
 
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.awt.event.KeyListener;
+
 public class Controller {
+
+    public Controller(Scene h){
+        Scene home = h;
+        System.out.println("enterControllerClass");
+        home.setOnKeyPressed(keyListener);
+    }
+
+    final EventHandler<KeyEvent> keyListener = new EventHandler<KeyEvent>() {
+        @Override
+        public void handle(javafx.scene.input.KeyEvent e){
+            switch(e.getCode()) {
+                case Z :
+                    System.out.println("Z");
+                    break;
+                case Q :
+                    System.out.println("Q");
+                    break;
+                case S :
+                    System.out.println("S");
+                    break;
+                case D :
+                    System.out.println("D");
+                    break;
+                default:
+                    return;
+
+            }
+        }
+    };
+
+
+
 }
+
+
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,11 +1,16 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
@@ -24,9 +29,16 @@ public class Main extends Application {
 
         // Init primaryStage
         Scene home = new Scene(root, 600, 400, true);
+
+        // init controles du joueur
+        Controller cont = new Controller(home);
+
+
         primaryStage.setTitle("Survival");
         primaryStage.setScene(home);
         primaryStage.show();
+
+
 
 
     }
